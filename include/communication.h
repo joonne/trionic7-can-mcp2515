354970ccffb2f63ebc6a9c9cd368af6013d8131e
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2024-02-03 22:45:21 +0200

    feature: initial version, added cdc handshake & heartbeat functionalities

--- include/communication.h
+++ include/communication.h
@@ -1,44 +1,43 @@
 #pragma once
 
-enum SID_MESSAGE
-{
-    ORDER,
-    IDK,
-    ROW,
-    LETTER0,
-    LETTER1,
-    LETTER2,
-    LETTER3,
-    LETTER4
+enum SID_MESSAGE {
+  ORDER,
+  IDK,
+  ROW,
+  LETTER0,
+  LETTER1,
+  LETTER2,
+  LETTER3,
+  LETTER4
 };
 
-enum class CAN_ID : unsigned long
-{
-    IBUS_BUTTONS = 0x290,
-    RADIO_MSG = 0x328,
-    RADIO_PRIORITY = 0x348,
-    O_SID_MSG = 0x33F,
-    O_SID_PRIORITY = 0x358,
-    TEXT_PRIORITY = 0x368,
-    LIGHTING = 0x410,
-    SPEED_RPM = 0x460
+enum class CAN_ID : unsigned long int {
+  STEERING_WHEEL_BUTTONS = 0x290,
+  RADIO_MSG = 0x328,
+  RADIO_PRIORITY = 0x348,
+  O_SID_MSG = 0x33F,
+  O_SID_PRIORITY = 0x358,
+  TEXT_PRIORITY = 0x368,
+  LIGHTING = 0x410,
+  SPEED_RPM = 0x460,
+  CDC_HEARTBEAT = 0x3C8,
+  CDC_HANDSHAKE_REQUEST = 0x6A1,
+  CDC_HANDSHAKE_RESPONSE = 0x6A2,
 };
 
-enum class STEERING_WHEEL : unsigned char
-{
-    NXT = 2,
-    SEEK_DOWN = 3,
-    SEEK_UP = 4,
-    SRC = 5,
-    VOL_UP = 6,
-    VOL_DOWN = 7
+enum class STEERING_WHEEL : unsigned char {
+  NXT = 2,
+  SEEK_DOWN = 3,
+  SEEK_UP = 4,
+  SRC = 5,
+  VOL_UP = 6,
+  VOL_DOWN = 7
 };
 
-enum class SID_BUTTON : unsigned char
-{
-    NPANEL = 3,
-    UP = 4,
-    DOWN = 5,
-    SET = 6,
-    CLR = 7
+enum class SID_BUTTON : unsigned char {
+  NPANEL = 3,
+  UP = 4,
+  DOWN = 5,
+  SET = 6,
+  CLR = 7
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,64 @@
-/*
-  By Reko Meriö
-  github.com/rekomerio
-*/
-
-// TODO: pair the raspi as cd changer
-
-#include "SidMessageHandler/SidMessageHandler.h"
 #include "communication.h"
 #include "defines.h"
 #include "headers.h"
 #include "mcp_can.h"
+#include "mcp_can_dfs.h"
 #include <Arduino.h>
 
 MCP_CAN CAN(CAN_CS_PIN);
-SidMessageHandler sidMessageHandler(&CAN);
-
-bool isBluetoothEnabled;
-
-struct {
-  uint32_t clearLastPressedAt = 0;
-  uint32_t setLastPressedAt = 0;
-} sidButtons;
 
 void setup() {
-  isBluetoothEnabled = false;
-#if DEBUG
   Serial.begin(115200);
-#endif
-  pinMode(BUTTON_PIN, INPUT);
-  pinMode(BLUETOOTH_PIN0, OUTPUT);
-  pinMode(BLUETOOTH_PIN1, OUTPUT);
-  pinMode(TRANSISTOR_PIN, OUTPUT);
   while (CAN.begin(MCP_ANY, I_BUS, MCP_8MHZ) != CAN_OK) {
     delay(100);
   }
   CAN.setMode(MCP_NORMAL);
 }
 
-void loop() {
-  readCanBus();
-  sidMessageHandler.update();
+void loop() { readCanBus(); }
+
+unsigned char CDC_HEARTBEAT_CMD[8] = {0xE0, 0x00, 0x3F, 0x31,
+                                      0xFF, 0xFF, 0xFF, 0xD0};
+unsigned char CDC_HANDSHAKE_CMD[8] = {0x62, 0x00, 0x00, 0x16,
+                                      0x01, 0x02, 0x00, 0x00};
+
+void cdcHeartbeat() {
+  CAN.sendMsgBuf(static_cast<unsigned long>(CAN_ID::CDC_HEARTBEAT), 8,
+                 CDC_HEARTBEAT_CMD);
 }
 
-void toggleBluetooth() {
-  isBluetoothEnabled = !isBluetoothEnabled;
-  digitalWrite(BLUETOOTH_PIN0, isBluetoothEnabled);
-  digitalWrite(BLUETOOTH_PIN1, isBluetoothEnabled);
-  digitalWrite(TRANSISTOR_PIN, isBluetoothEnabled);
-}
-
-void nextTrack() {
-  sidMessageHandler.sendMessage("NEXT TRACK", 800);
-  pinMode(BT_NEXT, OUTPUT);
-  digitalWrite(BT_NEXT, LOW);
-  delay(70);
-  pinMode(BT_NEXT, INPUT);
-}
-
-void previousTrack() {
-  sidMessageHandler.sendMessage("PREVIOUS TRACK", 800);
-  pinMode(BT_PREVIOUS, OUTPUT);
-  digitalWrite(BT_PREVIOUS, LOW);
-  delay(70);
-  pinMode(BT_PREVIOUS, INPUT);
+void cdcHandshake() {
+  CAN.sendMsgBuf(static_cast<unsigned long>(CAN_ID::CDC_HANDSHAKE_RESPONSE), 8,
+                 CDC_HANDSHAKE_CMD);
 }
 
 void readCanBus() {
   uint8_t len;
   uint8_t data[8];
-  long unsigned id;
+  unsigned long id;
 
-  if (CAN.checkReceive() == CAN_MSGAVAIL) {
-    CAN.readMsgBuf(&id, &len, data);
+  cdcHeartbeat();
+
+  if (CAN.checkReceive() == CAN_NOMSG) {
+    DEBUG_MESSAGE("No message received");
+    return;
+  }
+
+  if (CAN.readMsgBuf(&id, &len, data) == CAN_OK) {
     switch (static_cast<CAN_ID>(id)) {
-    case CAN_ID::IBUS_BUTTONS: {
-      uint8_t action = getHighBit(data[AUDIO]);
-      steeringWheelActions(static_cast<STEERING_WHEEL>(action));
-
-      action = getHighBit(data[SID]);
-      sidActions(static_cast<SID_BUTTON>(action));
-      break;
+    case CAN_ID::CDC_HANDSHAKE_REQUEST: {
+      DEBUG_MESSAGE("CD_CHANGER_REQUEST");
+      return cdcHandshake();
     }
-    case CAN_ID::LIGHTING:
-      lightActions(data);
-      break;
-    case CAN_ID::SPEED_RPM:
-      vehicleActions(data);
-      break;
-    case CAN_ID::TEXT_PRIORITY:
-      sidMessageHandler.setPriority(data[0], data[1]);
-      break;
-    case CAN_ID::RADIO_MSG:
-      sidMessageHandler.onReceive(id, data);
-      break;
+    case CAN_ID::STEERING_WHEEL_BUTTONS: {
+      auto action = getHighBit(data[AUDIO]);
+      DEBUG_MESSAGE("IBUS_BUTTONS: " + String(action));
+      return steeringWheelActions(static_cast<STEERING_WHEEL>(action));
+    }
+    default: {
+      DEBUG_MESSAGE("Unknown CAN ID: " + String(id));
+      return;
+    }
     }
   }
 }
@@ -103,19 +69,12 @@ void steeringWheelActions(STEERING_WHEEL action) {
     DEBUG_MESSAGE("NEXT");
     break;
   case STEERING_WHEEL::SEEK_DOWN:
-    if (isBluetoothEnabled) {
-      previousTrack();
-    }
     DEBUG_MESSAGE("SEEK DOWN");
     break;
   case STEERING_WHEEL::SEEK_UP:
-    if (isBluetoothEnabled) {
-      nextTrack();
-    }
     DEBUG_MESSAGE("SEEK UP");
     break;
   case STEERING_WHEEL::SRC:
-    toggleBluetooth();
     DEBUG_MESSAGE("SRC");
     break;
   case STEERING_WHEEL::VOL_UP:
@@ -127,37 +86,6 @@ void steeringWheelActions(STEERING_WHEEL action) {
   }
 }
 
-void sidActions(SID_BUTTON action) {
-  switch (action) {
-  case SID_BUTTON::NPANEL:
-    DEBUG_MESSAGE("NIGHT PANEL");
-    break;
-  case SID_BUTTON::UP:
-    DEBUG_MESSAGE("UP");
-    break;
-  case SID_BUTTON::DOWN:
-    DEBUG_MESSAGE("DOWN");
-    break;
-  case SID_BUTTON::SET:
-    if (elapsed(sidButtons.setLastPressedAt) < 500) {
-      sidMessageHandler.sendMessage("LEDS ON", 500);
-      DEBUG_MESSAGE("SET DOUBLETAP");
-    }
-    sidButtons.setLastPressedAt = millis();
-    DEBUG_MESSAGE("SET");
-    break;
-  case SID_BUTTON::CLR:
-    if (elapsed(sidButtons.clearLastPressedAt) < 500) {
-      sidMessageHandler.sendMessage("LEDS OFF", 500);
-      DEBUG_MESSAGE("CLEAR DOUBLETAP");
-    }
-    sidMessageHandler.cancelMessage();
-    sidButtons.clearLastPressedAt = millis();
-    DEBUG_MESSAGE("CLEAR");
-    break;
-  }
-}
-
 /*
   Checks every bit of the given value until finds the first high bit and then
   returns the position of that; or if none are high, returns 0xFF (255).
@@ -165,19 +93,15 @@ void sidActions(SID_BUTTON action) {
   @return - first high bit of the given value or 0xFF
 */
 uint8_t getHighBit(uint8_t value) {
-  if (!value)
+  if (!value) {
     return 0xFF;
+  }
 
   for (uint8_t i = 0; i < 8; i++) {
     if (value >> i & 1) {
       return i;
     }
   }
+
   return 0xFF;
 }
-
-uint16_t combineBytes(uint8_t byte1, uint8_t byte2) {
-  return (byte1 << 8 | byte2);
-}
-
-uint32_t elapsed(uint32_t time) { return millis() - time; }
